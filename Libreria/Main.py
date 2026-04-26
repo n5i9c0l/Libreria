ca4be6bcf6286186ef29439939b814e0d50f540e
mati <mati13752933@gmail.com>
2026-04-26 18:20:52 -0400

    Update Main.py

--- Libreria/Main.py
+++ Libreria/Main.py
@@ -1,1 +1,12 @@
-
+from Libreria.IA.gemini import Ia
+from Libreria.Audio.audio import Audio
+audiouwu = Audio()
+iaunu = Ia()
+try:
+    texto = audiouwu.escuchar()
+    if texto:
+        respuesta = iaunu.generar(texto)
+        print("IA respondió: " + respuesta)
+        audiouwu.hablar(respuesta)
+except Exception as e:
+    print("Ocurrió un error")
