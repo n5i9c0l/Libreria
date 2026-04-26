0a533061b99f0dbfd28e9bab42d2d2bcfabef134
mati <mati13752933@gmail.com>
2026-04-26 18:32:11 -0400

    Update Main.py

--- Libreria/Main.py
+++ Libreria/Main.py
@@ -1,12 +1,37 @@
-from Libreria.IA.gemini import Ia
+from Libreria.IA.openRouter import OpenRouter
 from Libreria.Audio.audio import Audio
 audiouwu = Audio()
-iaunu = Ia()
-try:
+ia = OpenRouter()
+audiouwu.hablar("¿Quieres usar teclado o micrófono?")
+opcion = input("¿Quieres usar teclado o micrófono? ").lower()
+#imaginemos que no es ciego, entonces que diga qué quiere usar(microfono o tecaldo)
+if opcion == "teclado":#si es teclado
+    texto = input("¿Qué quieres preguntarle a la IA? ")
+    respuesta = ia.generar(texto)
+    print("IA respondió: " + respuesta)
+    audiouwu.hablar(respuesta)
+elif opcion == "microfono":#si es micro
     texto = audiouwu.escuchar()
     if texto:
-        respuesta = iaunu.generar(texto)
+        respuesta = ia.generar(texto)
         print("IA respondió: " + respuesta)
         audiouwu.hablar(respuesta)
-except Exception as e:
-    print("Ocurrió un error")
+else:#si es que puso otra cosa >:v
+    #tal vez es ciego y por eso puso alguna wbda, entonces que hable
+    audiouwu.hablar("No entendí, dime por micrófono si quieres teclado o micrófono")
+    print("No entendí, dime por micrófono si quieres teclado o micrófono")
+    print("Habla: ")
+    texto = audiouwu.escuchar()
+    if texto and "teclado" in texto.lower(): #si dijo teclado
+        pregunta = input("¿Qué quieres preguntarle a la IA? ")
+        respuesta = ia.generar(pregunta)
+        print("IA respondió: " + respuesta)
+        audiouwu.hablar(respuesta)
+    elif texto and "microfono" in texto.lower():#si dijo micro
+        texto = audiouwu.escuchar()
+        if texto:
+            respuesta = ia.generar(texto)
+            print("IA respondió: " + respuesta)
+            audiouwu.hablar(respuesta)
+    else:#sino tampoco dijo algo válido :c
+        print("No se pudo entender :c")
