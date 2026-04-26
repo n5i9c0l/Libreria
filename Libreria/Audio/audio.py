d6ff68b64c66b1d84339d168656c2abd5c10fd9d
mati <mati13752933@gmail.com>
2026-04-26 12:54:07 -0400

    Update audio.py

--- Libreria/Audio/audio.py
+++ Libreria/Audio/audio.py
@@ -1,23 +1,24 @@
 import speech_recognition as sr
 import pyttsx3
-
-class MiAudio:
+class Audio:
     def __init__(self):
         self.reconocedor = sr.Recognizer()
         self.motor = pyttsx3.init()
-
     def escuchar(self):
-        with sr.Microphone() as fuente:
+        with sr.Microphone() as source:
             print("Empiece a realizar el dictado:")
-            audio = self.reconocedor.listen(fuente)
+            audio = self.reconocedor.listen(source)
         try:
-            texto = self.reconocedor.recognize_google(audio, language="es-ES")
-            print("Dijiste: " + texto)
-            return texto
-        except:
+            text = self.reconocedor.recognize_google(audio)
+            print("Ud. dijo: " + text)
+            return text
+        except sr.UnknownValueError:
             print("No puedo entender el audio.")
             return None
-
+        except sr.RequestError as e:
+            print("Error en los resultados; {0}".format(e))
+            return None
     def hablar(self, texto):
+        self.motor.setProperty('rate', 150)
         self.motor.say(texto)
         self.motor.runAndWait()
